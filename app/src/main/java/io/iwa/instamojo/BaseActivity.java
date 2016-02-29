2f708104a1480b136b87d7b20d5d912c94d05137
Harshit Pathak <harshit@houzify.com>
2016-02-29 05:46:07 +0530

    adding time analytics for activity

--- app/src/main/java/io/iwa/instamojo/BaseActivity.java
+++ app/src/main/java/io/iwa/instamojo/BaseActivity.java
@@ -4,15 +4,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.batch.android.Batch;
+
+import java.util.Date;
 
 /**
  * //TODO Add Class Description
  * Author: harshit  on 29/2/16.
  */
 public class BaseActivity extends AppCompatActivity{
-
+    long startTime;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,15 +25,17 @@ public class BaseActivity extends AppCompatActivity{
     protected void onStart()
     {
         super.onStart();
-
         Batch.onStart(this);
+        startTime = new Date().getTime();
     }
 
     @Override
     protected void onStop()
     {
         Batch.onStop(this);
-
+        long endTime = new Date().getTime();
+        Toast.makeText(getApplicationContext(),"Total Time Spent in Activity: " +
+                String.valueOf(endTime-startTime)+"ms",Toast.LENGTH_LONG).show();
         super.onStop();
     }
 
